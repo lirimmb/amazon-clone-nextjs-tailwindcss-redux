4f3c74d54ad1487d065d990b4ceec577da920f02
lirimmb <64708387+lirimmb@users.noreply.github.com>
2021-07-26 15:14:33 +0200

    half of header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import Image from "next/image"
 import {
     MenuIcon,
     SearchIcon,
-    ShoppingCarIcon
+    ShoppingCartIcon
 } from "@heroicons/react/outline"
 
 
@@ -11,6 +11,8 @@ const Header = () => {
         <header>
             {/*Top nav */}
             <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+
+                {/*Image */}
                 <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
                     <Image 
                         src='https://links.papareact.com/f90'
@@ -22,11 +24,36 @@ const Header = () => {
                 </div>
 
                 {/*Search bar */}
-                <div className="bg-yellow-400">
-                    <input type="text" />
+                <div className="hidden sm:flex items-center h-10 rounded-md flex-grow bg-yellow-400 hover:bg-yellow-500 cursor-pointer">
+                    <input className='p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4' type="text" />
                     <SearchIcon className="h-12 p-4" />
                 </div>
+
+                {/* Right side*/}
+                <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+                    <div className="link">
+                        <p>Hello Lirim</p>
+                        <p className="font-extrabold md:text-sm">Account & Lists</p>
+                    </div>
+
+                    <div className="link">
+                        <p>Return</p>
+                        <p className="font-extrabold md:text-sm">& Orders</p>
+                    </div>
+
+                    <div className="relative link flex items-center">
+
+                        <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
+
+                        <ShoppingCartIcon className="h-10"/>
+                        <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
+                    </div>
+                </div>
+
+
             </div>
+
+
             {/* Bottom nav */}
             <div></div>
         </header>
